c3c09755bc62168ba70c4f4ebade573b3c9212f4
Iya Hab <iya.gabaraeva@gmail.com>
2020-04-06 16:23:37 +0300

    change task 10

--- HW-10/HW-10-03/HW-10-03-04/main.js
+++ HW-10/HW-10-03/HW-10-03-04/main.js
@@ -1,6 +1,6 @@
 let year = prompt('Enter the year.');
 
-(year % 4 == 0) || (year % 400 == 0 ) && (year % 100 != 0) ? alert( year + ' is a leap year.' ) : alert( year + ' is not a leap year.' );
+(year % 400 == 0) || (year % 4 == 0 ) && (year % 100 != 0) ? alert( year + ' is a leap year.' ) : alert( year + ' is not a leap year.' );
 
 
 
